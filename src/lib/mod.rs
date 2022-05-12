9144a4175355a95dd3686be9117e715471e43f26
Rafael Lúcio <rafaellucio@pm.me>
2022-05-11 21:58:28 -0300

    chore: Run rustfmt

--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -23,7 +23,11 @@ impl Route {
 
 impl Display for Route {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Route to {} from {}, costing {}", self.src, self.dst, self.delta)
+        write!(
+            f,
+            "Route to {} from {}, costing {}",
+            self.src, self.dst, self.delta
+        )
     }
 }
 
@@ -40,18 +44,31 @@ mod route_tests {
     #[test]
     fn instantiation_should_fail_if_distance_is_zero() {
         let attempt_route = Route::new(String::from("A"), String::from("B"), 0);
-        assert_eq!(attempt_route, Err("A route with zero distance is a route to itself"));
+        assert_eq!(
+            attempt_route,
+            Err("A route with zero distance is a route to itself")
+        );
     }
 
     #[test]
-    fn instantiation_should_complete_if_parameters_are_acceptable(){
+    fn instantiation_should_complete_if_parameters_are_acceptable() {
         let attempt_route = Route::new(String::from("A"), String::from("B"), 1);
-        assert_eq!(attempt_route, Ok(Route { src: "A".to_string(), dst: "B".to_string(), delta: 1 }))
+        assert_eq!(
+            attempt_route,
+            Ok(Route {
+                src: "A".to_string(),
+                dst: "B".to_string(),
+                delta: 1
+            })
+        )
     }
 
     #[test]
     fn format_should_be_as_expected() {
         let route = Route::new(String::from("A"), String::from("B"), 1);
-        assert_eq!(format!("{}", route.ok().unwrap()), "Route to A from B, costing 1")
+        assert_eq!(
+            format!("{}", route.ok().unwrap()),
+            "Route to A from B, costing 1"
+        )
     }
 }
