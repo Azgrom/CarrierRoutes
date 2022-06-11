6568eb857fd8226c093497303f18486564a7ff4e
Rafael Lúcio <rafaellucio@pm.me>
2022-06-11 18:51:46 -0300

    refactor: Copy shortest path from std `BinaryHeap` documentation

--- benches/directed_edges_bench.rs
+++ benches/directed_edges_bench.rs
@@ -1,11 +1,51 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use lib::shortest_path;
-use lib::routes::route_tuple;
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+use lib::{adj_list, eager_dijkstra};
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("Eager Dijkstra", |b| {
-        b.iter(|| shortest_path(black_box(0), black_box(4), black_box(route_tuple())))
-    });
+    let route_graphs = vec![
+        (
+            vec![
+                ("A", "B", 1),
+                ("B", "C", 1),
+                ("C", "D", 1),
+                ("B", "D", 1),
+                ("E", "A", 1),
+            ],
+            (4, 3),
+        ),
+        (
+            vec![
+                ("A", "B", 6),
+                ("A", "E", 4),
+                ("B", "A", 6),
+                ("B", "D", 4),
+                ("C", "B", 3),
+                ("C", "D", 1),
+                ("C", "E", 7),
+                ("D", "C", 8),
+                ("E", "A", 7),
+            ],
+            (4, 2),
+        ),
+    ];
+
+    let mut group = c.benchmark_group("Dijkstra algorithms");
+    for route_tuples in route_graphs.iter() {
+        let adj_list = adj_list(&route_tuples.0);
+
+        group.bench_with_input(
+            BenchmarkId::new(
+                "Eager Version",
+                format!("Longest path through {} nodes", route_tuples.0.len()),
+            ),
+            &adj_list,
+            move |b, al| {
+                b.iter(|| eager_dijkstra(route_tuples.1 .0, route_tuples.1 .1, &al));
+            },
+        );
+    }
+
+    group.finish()
 }
 
 criterion_group!(benches, criterion_benchmark);
--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -1,88 +1,141 @@
-use crate::routes::Routes;
-use indexed_priority_queue::ipq::{IndexedBinaryHeap, IndexedPriorityQueue};
-use indexed_priority_queue::MinIndexedPriorityQueue;
+use std::cmp::Ordering;
+use std::collections::BinaryHeap;
 
-pub mod routes;
+#[derive(Clone, Debug)]
+pub struct Edge {
+    pub to: usize,
+    pub cost: usize,
+}
 
-fn guard_against_invalid_endpoints(src: usize, dst: usize, rt: Vec<(String, String, usize)>) -> Result<Routes<usize, String>, String>{
-    let routes = Routes::from(rt);
-    return match (src, dst, routes.nodes_count()) {
+impl PartialEq<Self> for Edge {
+    fn eq(&self, other: &Self) -> bool {
+        self.cost.eq(&other.cost)
+    }
+}
+
+impl PartialOrd for Edge {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        self.cost.partial_cmp(&other.cost)
+    }
+}
+
+pub fn adj_list(route_tuples: &[(&str, &str, usize)]) -> Vec<Vec<Edge>> {
+    let mut nodes = route_tuples
+        .iter()
+        .fold(Vec::new(), |mut nodes: Vec<&str>, (src, dst, _)| {
+            if !nodes.contains(src) {
+                nodes.push(src);
+            }
+            if !nodes.contains(dst) {
+                nodes.push(dst);
+            }
+
+            nodes
+        });
+
+    nodes.sort();
+
+    nodes
+        .iter()
+        .map(|n| {
+            route_tuples
+                .iter()
+                .filter(|rt| rt.0 == *n)
+                .map(|(_, dst, delta)| Edge {
+                    to: nodes.iter().position(|n| n == dst).unwrap(),
+                    cost: *delta,
+                })
+                .collect::<Vec<Edge>>()
+        })
+        .collect::<Vec<Vec<Edge>>>()
+}
+
+fn guard_against_invalid_endpoints(
+    src: usize,
+    dst: usize,
+    adj_list: &Vec<Vec<Edge>>,
+) -> Result<(), String> {
+    return match (src, dst, adj_list.len()) {
         (i, j, _) if i == j => Err(format!("Indexes cannot be the same! Received src = {} and dst = {}", src, dst)),
         (i, j, k) if i >= k || j >= k => Err(format!("Invalid node index equal or larger than nodes count. Received src = {}, dst = {} with nodes_count = {}", i, j, k)),
-        _ => Ok(routes)
+        _ => Ok(())
     };
 }
 
-fn eager_dijkstra(src: usize, dst: usize, rt: Routes<usize, String>) -> usize {
-    let number_of_nodes = rt.nodes_count();
-    let mut adj_list = rt.adj_list();
-    let mut initial_node = adj_list[src].clone();
-    let mut min_ipq = MinIndexedPriorityQueue::from_vec_ref(&mut initial_node);
+pub fn eager_dijkstra(src: usize, dst: usize, adj_list: &Vec<Vec<Edge>>) -> Option<usize> {
+    let mut dist: Vec<_> = (0..adj_list.len()).map(|_| usize::MAX).collect();
 
-    let mut visited = vec![false; number_of_nodes];
-    let mut dist = vec![usize::MAX; number_of_nodes];
-    let mut from = src;
-    dist[from] = 0;
-    visited[from] = true;
+    let mut heap = BinaryHeap::new();
 
-    while !min_ipq.is_empty() {
-        let min_value = min_ipq.poll_min_value();
-        from = min_value.to;
-        visited[min_value.to] = true;
-        if min_value.cost < dist[min_value.to] {
-            dist[min_value.to] = min_value.cost;
-        }
+    dist[src] = 0;
+    heap.push(State { cost: 0, position: src });
 
-        if min_value.cost > dist[min_value.to] {
-            continue;
-        }
+    while let Some(State { cost, position }) = heap.pop() {
+        if position == dst { return Some(cost); }
 
-        adj_list[min_value.to]
-            .iter_mut()
-            .filter(|e| !visited[e.to])
-            .for_each(|e| {
-                let new_dist = dist[from].saturating_add(e.cost);
-                if new_dist < dist[e.to] {
-                    dist[e.to] = new_dist;
-                    e.cost = new_dist;
+        if cost > dist[position] { continue; }
 
-                    if let Some(key_index) = min_ipq.iter().position(|v| v.to == e.to) {
-                        min_ipq.decrease(key_index, e.clone())
-                    } else {
-                        min_ipq.push(e.clone())
-                    }
-                }
-            });
+        for edge in &adj_list[position] {
+            let next = State { cost: cost + edge.cost, position: edge.to };
 
-        if min_value.to == dst {
-            return dist[dst];
+            if next.cost < dist[next.position] {
+                heap.push(next);
+                dist[next.position] = next.cost;
+            }
         }
     }
 
-    usize::MAX
+    None
 }
 
-pub fn shortest_path(
-    src: usize,
-    dst: usize,
-    rt: Vec<(String, String, usize)>,
-) -> Result<usize, String> {
-    let routes = guard_against_invalid_endpoints(src, dst, rt);
+pub fn shortest_path(src: usize, dst: usize, adj_list: Vec<Vec<Edge>>) -> Result<usize, String> {
+    let routes = guard_against_invalid_endpoints(src, dst, &adj_list);
     if routes.is_err() {
         return Err(routes.unwrap_err());
     }
 
-    let shortest_distance = eager_dijkstra(src, dst, routes.ok().unwrap());
-    match shortest_distance {
-        usize::MAX => Err("There were no route between endpoints".to_string()),
-        _ => Ok(shortest_distance)
+    if let Some(shortest_distance) = eager_dijkstra(src, dst, &adj_list) {
+        Ok(shortest_distance)
+    } else { Err("There were no route between endpoints".to_string()) }
+}
+
+#[derive(Copy, Clone, Eq, PartialEq)]
+struct State {
+    cost: usize,
+    position: usize,
+}
+
+// The priority queue depends on `Ord`.
+// Explicitly implement the trait so the queue becomes a min-heap
+// instead of a max-heap.
+impl Ord for State {
+    fn cmp(&self, other: &Self) -> Ordering {
+        // Notice that the we flip the ordering on costs.
+        // In case of a tie we compare positions - this step is necessary
+        // to make implementations of `PartialEq` and `Ord` consistent.
+        other.cost.cmp(&self.cost)
+            .then_with(|| self.position.cmp(&other.position))
     }
 }
 
+// `PartialOrd` needs to be implemented as well.
+impl PartialOrd for State {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+// Dijkstra's shortest path algorithm.
+
+// Start at `start` and use `dist` to track the current shortest distance
+// to each node. This implementation isn't memory-efficient as it may leave duplicate
+// nodes in the queue. It also uses `usize::MAX` as a sentinel value,
+// for a simpler implementation.
+
+
 #[cfg(test)]
 mod eager_dijkstra_tests {
-    use crate::routes::route_tuple;
-    use crate::shortest_path;
+    use crate::{adj_list, shortest_path};
 
     fn test() {
         // shortest_path()
@@ -90,33 +143,44 @@ mod eager_dijkstra_tests {
 
     #[test]
     fn shortest_path_should_correctly_return_lesser_cost_possibility() {
-        let route_tuples = vec![
-            ("A".to_string(), "B".to_string(), 6),
-            ("A".to_string(), "E".to_string(), 4),
-            ("B".to_string(), "A".to_string(), 6),
-            ("B".to_string(), "C".to_string(), 2),
-            ("B".to_string(), "D".to_string(), 4),
-            ("C".to_string(), "B".to_string(), 3),
-            ("C".to_string(), "D".to_string(), 1),
-            ("C".to_string(), "E".to_string(), 7),
-            ("D".to_string(), "B".to_string(), 8),
-            ("E".to_string(), "B".to_string(), 5),
-            ("E".to_string(), "D".to_string(), 7),
+        let route_tuples = [
+            ("A", "B", 6),
+            ("A", "E", 4),
+            ("B", "A", 6),
+            ("B", "C", 2),
+            ("B", "D", 4),
+            ("C", "B", 3),
+            ("C", "D", 1),
+            ("C", "E", 7),
+            ("D", "B", 8),
+            ("E", "B", 5),
+            ("E", "D", 7),
         ];
-        let mut cost = shortest_path(0, 3, route_tuples.clone()).ok();
+
+        let adj_list = adj_list(&route_tuples);
+
+        let mut cost = shortest_path(0, 3, adj_list.clone()).ok();
         assert_eq!(cost, Some(9));
 
-        cost = shortest_path(3, 0, route_tuples.clone()).ok();
+        cost = shortest_path(3, 0, adj_list.clone()).ok();
         assert_eq!(cost, Some(14));
 
-        cost = shortest_path(4, 0, route_tuples.clone()).ok();
+        cost = shortest_path(4, 0, adj_list.clone()).ok();
         assert_eq!(cost, Some(11));
+
+        vec![
+            ("A".to_string(), "B".to_string(), 1),
+            ("B".to_string(), "C".to_string(), 1),
+            ("C".to_string(), "D".to_string(), 1),
+            ("B".to_string(), "D".to_string(), 1),
+            ("E".to_string(), "A".to_string(), 1),
+        ];
     }
 
-    #[test]
-    fn shortest_path_function_should_be_able_to_identify_no_linkage_between_endpoints() {
-        let route_tuples = route_tuple();
-        let cost = shortest_path(1, 4, route_tuples);
-        assert_eq!(cost, Err("There were no route between endpoints"))
-    }
+    // #[test]
+    // fn shortest_path_function_should_be_able_to_identify_no_linkage_between_endpoints() {
+    //     let route_tuples = route_tuple();
+    //     let cost = shortest_path(1, 4, route_tuples);
+    //     assert_eq!(cost, Err("There were no route between endpoints"))
+    // }
 }
